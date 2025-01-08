e955bce41fe916eb7f549f9ce311cda6529cf7f1
Junior <ferrazjunior97@gmail.com>
2025-01-08 06:45:15 -0300

    add pre_config.py for multipart upload and update config.js with new APS_MODEL_URN

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-export const APS_MODEL_URN = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6anNvbi1pb3QtdHV0b3JpYWwtdGVzdC9TYWxhJTIwZGUlMjBEZXNjYW5zby5ydnQ';
+export const APS_MODEL_URN = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6cHJvamVjdC0wMDkyZWJkMC9zYWxhX2RlX2Rlc2NhbnNvLnJ2dA';
 export const APS_MODEL_VIEW = '9f3c7d7b-9f16-0f6c-421a-e619ac1d7c0e';
 export const APS_MODEL_DEFAULT_FLOOR_INDEX = 2;
 // Datatime padrao é o dia de hoje das 00:00 até 23:59
